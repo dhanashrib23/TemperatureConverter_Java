98f600ec2c6675e114dad1a0dff3d9ac481afc6f
Dhanashri Bhosale <dhanashribhosale2002@gmail.com>
2023-07-26 11:45:49 +0530

    The function FahrenheitToKelvin is added

--- GitPractice.java
+++ GitPractice.java
@@ -6,11 +6,17 @@ public class GitPractice {
         System.out.print("Enter the temperature in Fahrenheit : ");
         double Fahrenheit = sc.nextDouble();
         FahrenheitToDegree(Fahrenheit);
+        FahrenheitToKelvin(Fahrenheit);
     }
 
     public static void FahrenheitToDegree(double Fahrenheit) {
         double degree = (Fahrenheit - 32) * 5 / 9;
         System.out.println("The temperature in degree celcius is : " + degree);
+    }
+
+    public static void FahrenheitToKelvin(double Fahrenheit) {
+        double kelvin = (Fahrenheit - 32) * (5 / 9) + 273.15;
+        System.out.println("The temperature in kelvin is : " + kelvin);
     }
 
 }
